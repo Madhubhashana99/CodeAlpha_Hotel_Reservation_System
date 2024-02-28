330c1e8272e81d3d6e7c88049c7eb7c1e614e2d8
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-02-28 11:22:47 +0530

    Reservation class done

--- CodeAlpha_Hotel_Reservation_System/src/Reservation.java
+++ CodeAlpha_Hotel_Reservation_System/src/Reservation.java
@@ -1,2 +1,38 @@
+import java.util.Date;
+
+//Represent a booking made by a user
 public class Reservation {
+    private int reservationId;
+    private String userName;
+    private Room room;
+    private Date checkInDate;
+    private Date checkOutDate;
+
+    public Reservation(int reservationId, String userName, Room room, Date checkInDate, Date checkOutDate) {
+        this.reservationId = reservationId;
+        this.userName = userName;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
 }
